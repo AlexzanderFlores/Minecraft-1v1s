49f1376d896a7d5bdd0028b04ff810a882c30f2a
OutsideTheBlock <outsidetheblocknetwork@gmail.com>
2016-04-02 06:58:53 -0700

    Stopped working on the custom kit selection

--- src/ostb/server/servers/building/Building.java
+++ src/ostb/server/servers/building/Building.java
@@ -5,14 +5,10 @@ import java.io.File;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
 
 import ostb.OSTB;
 import ostb.ProPlugin;
-import ostb.gameapi.KitSelection;
 import ostb.player.MessageHandler;
 import ostb.player.account.AccountHandler.Ranks;
 import ostb.server.CommandBase;
@@ -50,14 +46,6 @@ public class Building extends ProPlugin {
 				return true;
 			}
 		}.setRequiredRank(Ranks.OWNER);
-		new CommandBase("test", true) {
-			@Override
-			public boolean execute(CommandSender sender, String [] arguments) {
-				Player player = (Player) sender;
-				new KitSelection(player).spawnNPCs();
-				return true;
-			}
-		};
 	}
 	
 	@Override
@@ -65,16 +53,5 @@ public class Building extends ProPlugin {
 		FileHandler.delete(new File(OSTB.getInstance().getDataFolder().getPath() + "/../Multiverse-Core/worlds.yml"));
 		FileHandler.delete(new File(OSTB.getInstance().getDataFolder().getPath() + "/../Essentials/userdata"));
 		super.disable();
-	}
-	
-	@EventHandler
-	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
-		if(event.getRightClicked() instanceof ItemFrame) {
-			event.setCancelled(true);
-			ItemFrame itemFrame = (ItemFrame) event.getRightClicked();
-			//new ImageMap(itemFrame, OSTB.getInstance().getDataFolder().getPath() + "/Mines.png");
-			Location loc = itemFrame.getLocation();
-			event.getPlayer().sendMessage(loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ());
-		}
 	}
 }
