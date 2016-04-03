37a511f9612fce34134556631476359efab8849a
OutsideTheBlock <outsidetheblocknetwork@gmail.com>
2016-04-02 20:12:26 -0700

    Progress on kit selection

--- src/ostb/server/servers/building/Building.java
+++ src/ostb/server/servers/building/Building.java
@@ -9,6 +9,20 @@ import org.bukkit.entity.Player;
 
 import ostb.OSTB;
 import ostb.ProPlugin;
+import ostb.gameapi.KitSelection;
+import ostb.gameapi.games.skywars.kits.Archer;
+import ostb.gameapi.games.skywars.kits.Bomber;
+import ostb.gameapi.games.skywars.kits.Builder;
+import ostb.gameapi.games.skywars.kits.CowSlayer;
+import ostb.gameapi.games.skywars.kits.Enchanter;
+import ostb.gameapi.games.skywars.kits.Enderman;
+import ostb.gameapi.games.skywars.kits.Fisherman;
+import ostb.gameapi.games.skywars.kits.Looter;
+import ostb.gameapi.games.skywars.kits.Medic;
+import ostb.gameapi.games.skywars.kits.Miner;
+import ostb.gameapi.games.skywars.kits.Ninja;
+import ostb.gameapi.games.skywars.kits.Pyro;
+import ostb.gameapi.games.skywars.kits.Spiderman;
 import ostb.player.MessageHandler;
 import ostb.player.account.AccountHandler.Ranks;
 import ostb.server.CommandBase;
@@ -16,6 +30,8 @@ import ostb.server.util.ConfigurationUtil;
 import ostb.server.util.FileHandler;
 
 public class Building extends ProPlugin {
+	private KitSelection selection = null;
+	
 	public Building() {
 		super("Building");
 		addGroup("24/7");
@@ -46,6 +62,34 @@ public class Building extends ProPlugin {
 				return true;
 			}
 		}.setRequiredRank(Ranks.OWNER);
+		new CommandBase("test", true) {
+			@Override
+			public boolean execute(CommandSender sender, String [] arguments) {
+				Player player = (Player) sender;
+				if(selection == null) {
+					selection = new KitSelection(player);
+				} else {
+					selection.delete();
+					selection = null;
+					return true;
+				}
+				selection.update();
+				return true;
+			}
+		};
+		new Archer();
+		new Builder();
+		new Looter();
+		new Enchanter();
+		new Bomber();
+		new Ninja();
+		new Medic();
+		new CowSlayer();
+		new Enderman();
+		new Fisherman();
+		new Spiderman();
+		new Pyro();
+		new Miner();
 	}
 	
 	@Override
