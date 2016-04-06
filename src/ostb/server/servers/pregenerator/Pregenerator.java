e39da8b88431eefba9517e685d857d5b71bdd2bf
OutsideTheBlock <OutsideTheBlock@users.noreply.github.com>
2016-04-06 00:46:02 -0700

    Create Pregenerator.java
    Started work on the pregenerator

---
+++ src/ostb/server/servers/pregenerator/Pregenerator.java
@@ -0,0 +1,10 @@
+package ostb.server.servers.pregenerator;
+
+import ostb.ProPlugin;
+
+public class Pregenerator extends ProPlugin {
+	public Pregenerator() {
+		super("Pregenerator");
+		new Events();
+	}
+}
