3b64b4fe4de5c955968c83461fe14affcce5e997
OutsideTheBlock <outsidetheblocknetwork@gmail.com>
2016-04-12 17:04:36 -0700

    Removed spinning cages

--- src/ostb/gameapi/games/skywars/SkyWars.java
+++ src/ostb/gameapi/games/skywars/SkyWars.java
@@ -9,9 +9,7 @@ import ostb.gameapi.MiniGame;
 import ostb.gameapi.games.skywars.cages.BigCage;
 import ostb.gameapi.games.skywars.cages.ColoredBigCage;
 import ostb.gameapi.games.skywars.cages.ColoredSmallCage;
-import ostb.gameapi.games.skywars.cages.ColoredSpinningCage;
 import ostb.gameapi.games.skywars.cages.SmallCage;
-import ostb.gameapi.games.skywars.cages.SpinningCage;
 import ostb.player.scoreboard.BelowNameHealthScoreboardUtil;
 import ostb.server.CommandBase;
 
@@ -31,18 +29,6 @@ public class SkyWars extends MiniGame {
 					new SmallCage(player);
 				} else if(arguments[0].equalsIgnoreCase("big")) {
 					new BigCage(player);
-				} else if(arguments[0].equalsIgnoreCase("spin")) {
-					Material material = null;
-					byte data = 0;
-					if(arguments.length == 2) {
-						material = Material.valueOf(arguments[1].toUpperCase());
-					} else if(arguments.length == 3) {
-						material = Material.valueOf(arguments[1].toUpperCase());
-						data = Byte.valueOf(arguments[2]);
-					} else {
-						return false;
-					}
-					new SpinningCage(player, material, data);
 				} else if(arguments[0].equalsIgnoreCase("coloredSmall")) {
 					Material material = null;
 					byte data = 0;
@@ -67,23 +53,6 @@ public class SkyWars extends MiniGame {
 						return false;
 					}
 					new ColoredBigCage(player, material, data);
-				} else if(arguments[0].equalsIgnoreCase("coloredSpinning")) {
-					Material spinningMaterial = null;
-					Material cageMaterial = null;
-					byte spinningData = 0;
-					byte cageData = 0;
-					if(arguments.length == 3) {
-						spinningMaterial = Material.valueOf(arguments[1].toUpperCase());
-						cageMaterial = Material.valueOf(arguments[2].toUpperCase());
-					} else if(arguments.length == 5) {
-						spinningMaterial = Material.valueOf(arguments[1].toUpperCase());
-						cageMaterial = Material.valueOf(arguments[3].toUpperCase());
-						spinningData = Byte.valueOf(arguments[2]);
-						cageData = Byte.valueOf(arguments[4]);
-					} else {
-						return false;
-					}
-					new ColoredSpinningCage(player, spinningMaterial, spinningData, cageMaterial, cageData);
 				} else if(arguments[0].equalsIgnoreCase("setMax")) {
 					OSTB.setMaxPlayers(Integer.valueOf(arguments[1]));
 				} else {
